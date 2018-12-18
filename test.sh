36c3dd3e3940fd65fbc8d9569b335f951122a64a
jvergara <jvergara@env.ethz.ch>
2018-12-18 10:42:39 +0100

    adapting testing script

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 
 cd
-rm -rf /scratch/snx3000/jvergara/cosmo-pompa_x_chain_v1.1_test
-git clone cosmo-pompa_x_chain_v1.1 /scratch/snx3000/jvergara/cosmo-pompa_x_chain_v1.1_test
-cd /scratch/snx3000/jvergara/cosmo-pompa_x_chain_v1.1_test/cosmo/test/climate/crClim2km_DVL
+rm -rf /scratch/snx3000/jvergara/cosmo_sandbox_test
+git clone cosmo_sandobox_x_chain /scratch/snx3000/jvergara/cosmo_sandbox_test
+cd /scratch/snx3000/jvergara/cosmo_sandbox_test
 python define_simulation.py
 python control_simulation.py
 cp /store/c2sm/pr04/jvergara/base_executables/* bin/
